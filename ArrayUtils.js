3eed42f9b30f8f6ccefd4d179b20534c0dffaf2e
Nick Satinover <nsatinover.aws@gmail.com>
2020-12-21 20:43:33 -0500

    solution

--- ArrayUtils.js
+++ ArrayUtils.js
@@ -1,21 +1,43 @@
 class ArrayUtils{
 
-    isEmpty(array){return false;}
+    isEmpty(array){
+        return array.length < 1;
+    }
 
-    append(original, value){return original;}
+    append(original, value){
+        return original.append(value);
+    }
 
-    clone(original){return original;}
+    clone(original){
+        return original.map( (x) => x);
+    }
 
-    subArray(original, from, to){return original;}
+    subArray(original, from, to){
+        return original.subArray(from + 1, to);
+    }
 
-    equals(arr1, arr2){return false;}
+    equals(arr1, arr2){
+        return JSON.stringify(arr1) === JSON.stringify(arr2);
+    }
 
-    fill(original, value){}
+    fill(original, value){
+        return original.forEach(a => {
+            a = value;
+        });
+    }
 
-    indexOf(original, value){return -1;}
+    indexOf(original, value){
+        return original.indexOf(value);
+    }
 
-    remove(original, value){return original;}
+    remove(original, value){
+        return original.splice(
+            original.indexOf(value), 1
+            );
+    }
 
-    reverse(original){}
+    reverse(original){
+        return original.reverse;
+    }
     
 }
